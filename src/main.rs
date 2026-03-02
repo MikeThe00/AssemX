6de746f73469521647adde9ef8002e0e4c1dc2aa
MikeThe00 <mikey.rodino@gmail.com>
2026-03-02 10:32:01 -0600

    Update: just to keep to the repo going this code is going to be replaxed

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,53 @@ use scanlex::{Scanner, Token};
 // walk-tree-interpreter in rust from java code in the book craftinginterpreters
 
 fn main() {
+    enum Ast_tree {
+        Binary_expr(Ast_binary),
+        Decl(Ast_decl)
+        //Statements
+        //Expressions
+        //Literals
+        //Identifiers/Names
+    }
+    
+    //struct Ast_tree
+
+    //struct Ast1_module {
+     //module: String,
+     //in_scope: bool,
+     //string_of_bytes_to_run: String
+     //code later 
+     //this will run other modules (files) to use fn's or other things later
+    //}
+
+    struct Ast_decl {
+        is_var: bool,
+        is_module: bool
+        //code later for fun's 
+    }
+
+    struct Ast_binary {
+        left: String,
+        op: char,
+        right: i32
+    }
+
+    impl Ast_decl {
+       fn iden_find(token: &Token) -> Ast_decl{
+         if token == &Token::Iden("module".to_string()){
+         return Ast_decl{
+           is_module: true,
+           is_var: false
+          }
+         }
+         //else if code later
+        return Ast_decl{
+         is_module: false,
+         is_var: false
+        }
+       }
+    }
+
     fn run_files(path: &str) {
      let bytes: Vec<u8> = fs::read(path).expect("could not read file or there is no {path}");
      // should fix path with "/"s like Paths.get() in java
@@ -35,15 +82,35 @@ fn main() {
     }
 
     fn run(source: &str) {
-     //let mut tokens: Vec<Token> = Vec::new();
+     let mut tokens: Vec<Token> = Vec::new();
      let mut scan = Scanner::new(source);
      while let s = scan.get() {
          if s == Token::End {
           break
          }
-         println!("{:?}",s);
+         tokens.push(s);
      }
+     //debug
+     println!("{:?}", tokens);
      //code later
+     for i in 0..tokens.len() {
+        let n = &tokens[i];  
+        match n {
+          Token::Iden(_) => {
+              //if tokens[i] == Token::Iden("module".to_string()){
+              //debug
+               println!("Iden found");
+               let find = Ast_decl::iden_find(n);
+               //Ast_tree::Decl(find);
+               let found: bool = find.is_module;
+               println!("{}", find.is_module);
+              //code later
+          },
+          _ => todo!()
+          //code later
+        }
+     }
+     //for loop with enum and struct's
     }
 
     let args: Vec<String> = env::args().collect();
